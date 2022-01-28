a1770c21f3cbab16ae2ad13571b224dfc70fded0
Alon <Alonsouter@gmail.com>
2022-01-28 17:12:34 +0200

    AnimController

---
+++ Assets/Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour
+{
+    /// <summary>
+    /// Also handles INPUT? shouldn't really
+    /// </summary>
+    [SerializeField]
+    float moveSpeed;
+ 
+    
+    void Update()
+    {
+        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (inputVector.magnitude > 1)
+            inputVector.Normalize();
+
+        inputVector *= moveSpeed * Time.deltaTime;
+
+        transform.Translate(inputVector, Space.Self);
+    }
+}

bd292f38023b486d0896e0669299aa4ce9f15799
Amit Nino <amitnino@gmail.com>
2022-01-29 10:32:54 +0200

    health ui added

---
+++ Assets/Scripts/UI/HealthTxt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_Text))]
+public class HealthTxt : MonoBehaviour
+{
+    private TMP_Text _txtComponent;
+    public TMP_Text TxtComponent => _txtComponent;
+
+    [SerializeField]
+    private HealthData _playerHealthData;
+    public HealthData PlayerHealthData => _playerHealthData;
+
+    private void Awake()
+    {
+        _txtComponent = GetComponent<TMP_Text>();
+
+        _playerHealthData.EntityDamageEvent += setTxt;
+
+        setTxt();
+    }
+
+    private void setTxt()
+    {
+        _txtComponent.text = _playerHealthData.currentHealth.ToString();
+    }
+}
